38c76d8f3023953328077fef9bf2a7aa6ed1bc30
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-06-16 18:41:24 -0300

    ♻️ refactor: Update Home.vue to display company name in header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,34 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue';
+import { useCompany } from '~/stores/Companies';
+
+const companyStore = useCompany()
+const companies = computed(() => {
+  return companyStore.companies
+})
+const company = computed(() => {
+  return companyStore.company
+})
+
 </script>
 
 <template>
-  <div>
-    <h1 class="bg-red-500">
-      Hello World
-    </h1>
+  <div class="bg-white rounded p-2">
+    <div class="w-full flex justify-between">
+      <div class="flex gap-2 items-center">
+        <h1 class="text-xl font-semibold">
+          Ativos 
+        </h1>
+        <span class="text-3xl font-semibold">
+          /
+        </span>
+        <span class="text-gray-700">
+          {{ company }}
+        </span>
+      </div>
+      <div class="flex gap-x-2">
+        
+      </div>
+    </div>
   </div>
 </template>
