6bfbd05d009bb1a39d9e4d2bee74b6d858cb9c21
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-06-16 18:41:03 -0300

    ♻️ refactor: Update Layout.vue to use lazy loading for images

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,12 +1,25 @@
 <script setup lang="ts">
+import { onMounted, computed } from 'vue';
+import { useCompany } from '~/stores/Companies';
 import Navbar from '~/blocks/Navbar.vue'
 
+const companyStore = useCompany()
+const { execute: getCompanies } = companyStore.getCompanies()
+const companies = computed(() => {
+  return companyStore.companies.map((item) => {
+    return item.name
+  })
+})
+
+onMounted(() => {
+  getCompanies()
+})
 </script>
 
 <template>
   <div class="min-h-screen w-full flex flex-col">
-    <Navbar />
-    <main>
+    <Navbar :options="companies"/>
+    <main class="h-full min-h-screen items-start bg-slate-200 p-2">
       <router-view />
     </main>
   </div>
